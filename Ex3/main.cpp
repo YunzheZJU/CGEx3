0d5652ee5829f8fa61cb0435eaf06db913595f0a
Yunzhe <iLoveLive@honoka.moe>
2017-04-11 21:42:04 +0800

    Nothing important is changed.

--- Ex3/main.cpp
+++ Ex3/main.cpp
@@ -10,8 +10,8 @@
 #define Z 2
 
 float fTranslate;
-float fRotate    = 0.0f;
-float fScale     = 1.0f;	// set inital scale value to 1.0f
+float fRotate = 0.0f;
+float fScale = 1.0f;	// set inital scale value to 1.0f
 float fTpRtt = 0.0f;
 
 bool bPersp = false;
@@ -42,15 +42,16 @@ void Draw_Scene()
 	// teapot
 	glPushMatrix();
 	glTranslatef(target[0], target[1], target[2]);
-		glPushMatrix();
-		glTranslatef(0, 0, 4+1);
-		glRotatef(90, 1, 0, 0); //notice the rotation here, you may have a TRY removing this line to see what it looks like.
-		//hint: when operate the teapot, you may need to change some parameters
-		glRotatef(fTpRtt, 0, 1, 0);
-		glutSolidTeapot(1);
-		glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0, 0, 4 + 1);
+	glRotatef(90, 1, 0, 0); // notice the rotation here, you may have a TRY removing this line to see what it looks like.
+	// 以下横x纵y深z
+	// hint: when operate the teapot, you may need to change some parameters
+	glRotatef(fTpRtt, 0, 1, 0);
+	glutSolidTeapot(1);
 	glPopMatrix();
-	
+	glPopMatrix();
+
 	// table
 	glPushMatrix();
 	glTranslatef(0, 0, 3.5);
@@ -85,28 +86,28 @@ void Draw_Scene()
 
 void updateView(int width, int height)
 {
-	glViewport(0,0,width,height);						// Reset The Current Viewport
+	glViewport(0, 0, width, height);						// Reset The Current Viewport
 
 	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
 	glLoadIdentity();									// Reset The Projection Matrix
 
-	float whRatio = (GLfloat)width/(GLfloat)height;
+	float whRatio = (GLfloat)width / (GLfloat)height;
 
-	if (bPersp){
+	if (bPersp) {
 		//todo when 'p' operation, hint: use FUNCTION gluPerspective
 		gluPerspective(45.0f, whRatio, 0.1f, 100.0f);
 	}
 	else
-		glOrtho(-3 ,3, -3, 3,-100,100);
+		glOrtho(-3, 3, -3, 3, -100, 100);
 
 	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
 }
 
 void reshape(int width, int height)
 {
-	if (height==0)										// Prevent A Divide By Zero By
+	if (height == 0)										// Prevent A Divide By Zero By
 	{
-		height=1;										// Making Height Equal One
+		height = 1;										// Making Height Equal One
 	}
 
 	wHeight = height;
@@ -122,45 +123,45 @@ void idle()
 
 void key(unsigned char k, int x, int y)
 {
-	switch(k)
+	switch (k)
 	{
 	case 27:
 	case 'q': {exit(0); break; }
-	case 'p': {bPersp = !bPersp; updateView(wHeight, wWidth);break; }
+	case 'p': {bPersp = !bPersp; updateView(wHeight, wWidth); break; }
 
-	case ' ': {bAnim = !bAnim; break;}
-	case 'o': {bWire = !bWire; break;}
+	case ' ': {bAnim = !bAnim; break; }
+	case 'o': {bWire = !bWire; break; }
 
 	case 'a': {//hint: eye[] and center[] are the keys to solve the problems
 		eye[X] -= 0.1;
 		center[X] -= 0.1;
 		break;
-			  }
+	}
 	case 'd': {
 		eye[X] += 0.1;
 		center[X] += 0.1;
 		break;
-			  }
+	}
 	case 'w': {
 		eye[Y] += 0.1;
 		center[Y] += 0.1;
 		break;
-			  }
+	}
 	case 's': {
 		eye[Y] -= 0.1;
 		center[Y] -= 0.1;
 		break;
-			  }
+	}
 	case 'z': {
 		eye[Z] -= 0.2;
 		center[Z] -= 0.2;
 		break;
-			  }
+	}
 	case 'c': {
 		eye[Z] += 0.2;
 		center[Z] += 0.2;
 		break;
-			  }
+	}
 
 			  //茶壶相关操作
 	case 'l': {//hint:use the ARRAY that you defined, and notice the teapot can NOT be moved out the range of the table.
@@ -168,29 +169,29 @@ void key(unsigned char k, int x, int y)
 			target[X] += 0.2;
 		}
 		break;
-			  }
+	}
 	case 'j': {
 		if (target[X] >= -2) {
 			target[X] -= 0.2;
 		}
 		break;
-			  }
+	}
 	case 'i': {
 		if (target[Y] <= 1.5) {
 			target[Y] += 0.2;
 		}
 		break;
-			  }
+	}
 	case 'k': {
 		if (target[Y] >= -1.5) {
 			target[Y] -= 0.2;
 		}
 		break;
-			  }
+	}
 	case 'e': {
 		bRtt = !bRtt;
 		break;
-			  }
+	}
 	}
 }
 
@@ -215,7 +216,7 @@ void redraw()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light_pos[] = {5,5,5,1};
+	GLfloat light_pos[] = { 5,5,5,1 };
 
 	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
 	glLightfv(GL_LIGHT0, GL_AMBIENT, white);
@@ -226,21 +227,21 @@ void redraw()
 	glScalef(0.2, 0.2, 0.2);
 	Draw_Scene();							// Draw Scene
 
-	if (bAnim) fRotate    += 0.5f;
+	if (bAnim) fRotate += 0.5f;
 	if (bRtt) fTpRtt += 0.5f;
 	//todo; hint: when you want to rotate the teapot, you may like to add another line here =)
 	glutSwapBuffers();
 }
 
-int main (int argc,  char *argv[])
+int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	glutInitWindowSize(480,480);
+	glutInitWindowSize(480, 480);
 	int windowHandle = glutCreateWindow("Ex 3");
 
 	glutDisplayFunc(redraw);
-	glutReshapeFunc(reshape);	
+	glutReshapeFunc(reshape);
 	glutKeyboardFunc(key);
 	glutIdleFunc(idle);
 
